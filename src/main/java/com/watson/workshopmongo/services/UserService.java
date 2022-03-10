e0319f10169f942de539815c3eb940bcf991b849
WatsonAndrade <watsonleidson@gmail.com>
2022-03-10 16:17:39 -0300

    Obtendo um usuario por id

--- src/main/java/com/watson/workshopmongo/services/UserService.java
+++ src/main/java/com/watson/workshopmongo/services/UserService.java
@@ -1,12 +1,14 @@
 package com.watson.workshopmongo.services;
 
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 import com.watson.workshopmongo.domain.User;
 import com.watson.workshopmongo.repository.UserRepository;
+import com.watson.workshopmongo.services.exception.ObjectNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
@@ -16,5 +18,10 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+
+	public User findById(String id){
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
