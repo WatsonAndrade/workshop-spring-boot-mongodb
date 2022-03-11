8ac2a4cc7804a0374c61f3e3916893e6f47b62db
WatsonAndrade <watsonleidson@gmail.com>
2022-03-11 11:48:48 -0300

    Consulta simples com @Query

--- src/main/java/com/watson/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/watson/workshopmongo/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.watson.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.watson.workshopmongo.domain.Post;
@@ -11,4 +12,7 @@ import com.watson.workshopmongo.domain.Post;
 public interface PostRepository extends MongoRepository<Post, String> {
 
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ 'title': { $regex: ?0, $options: 'i'} }")
+	List<Post> searchitle(String text);
 }
