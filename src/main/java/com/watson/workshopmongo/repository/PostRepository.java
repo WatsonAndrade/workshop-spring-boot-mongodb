63e5cddbf0de65e9162f6da849812fd299fab84a
WatsonAndrade <watsonleidson@gmail.com>
2022-03-11 10:40:30 -0300

    Criando entity Post com user aninhado

---
+++ src/main/java/com/watson/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package com.watson.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.watson.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String> {
+
+}
